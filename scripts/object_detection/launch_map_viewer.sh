0d54288c29401571ae784fe0ac41ce5ccabde274
David Ross <ross.d2@northeastern.edu>
2025-12-12 01:19:03 -0800

    Update scripts to launch correct map visualizer. Change name of simulation to simulated

--- scripts/object_detection/launch_map_viewer.sh
+++ scripts/object_detection/launch_map_viewer.sh
@@ -4,4 +4,4 @@ echo "Sourcing ROS2 files..."
 source /opt/ros/jazzy/setup.bash
 source ../../main_ws/install/setup.bash
 echo "Running python script..."
-ros2 run object_location temp_viewer
+ros2 run object_location map_visualizer
